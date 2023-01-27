dd37bf35f0c7412535f62d2e024a3f391b5d9647
Ioana Tiplea <ioanatiplea94@gmail.com>
2023-01-27 11:06:02 +0200

    Complete sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,21 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    delete user.hash;
+
+    return user;
   }
 }
